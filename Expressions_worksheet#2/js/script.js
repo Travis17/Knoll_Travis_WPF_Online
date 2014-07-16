838d980fcd0a646776e547eda2383f06daf6e651
Travis Knoll <trav_k80@yahoo.com>
2014-07-15 17:26:14 -0700

    Slice of Pie#1

---
+++ Expressions_worksheet#2/js/script.js
@@ -0,0 +1,9 @@
+//Travis Knoll
+//Date: 15 July 2014
+
+var slicesPerPizza = 15;
+var peopleParty = 10;
+var pizzasOrdered = 10;
+
+//multiply number of slices times pizzas ordered to equal number per person.
+console.log(slicesPerPizza * pizzasOrdered / peopleParty);
