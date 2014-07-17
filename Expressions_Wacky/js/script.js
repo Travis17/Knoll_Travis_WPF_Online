ad7c23a691435fce1bdf74019a622d9f22605ff0
Travis Knoll <trav_k80@yahoo.com>
2014-07-17 10:28:56 -0700

    Alert what is the persons name?

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,8 @@
+//Travis Knoll
+//Date: 17 July 2014
+
+//Code to see how many hours a person spends in the bathroom in a year.
+
+//What is the persons name?
+var name = prompt("What is you name friend?");
+console.log(name);
