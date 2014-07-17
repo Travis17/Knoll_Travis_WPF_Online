56139c390583df808453c2b7b1d4653bcf7be5ec
Travis Knoll <trav_k80@yahoo.com>
2014-07-17 10:37:41 -0700

    Created a variable for how many times a person goes to the bathroom daily.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,3 +6,10 @@
 //What is the persons name?
 var name = prompt("What is you name friend?");
 console.log(name);
+
+//alert username and say hi. Ask if they want to see how much time they spend in the bathroom in a year.
+alert("Hi "+name+". Would you like to see how much time you spend in the bathroom a year?");
+
+//How many times do you go to the bathroom on any given day?
+var bathroomDay = prompt("On average how many times do you go to the bathroom daily?");
+console.log(bathroomDay);
