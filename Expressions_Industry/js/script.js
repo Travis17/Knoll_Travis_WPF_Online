94a053daa4980276eae07b8f268ff5dd8e543987
Travis Knoll <trav_k80@yahoo.com>
2014-07-17 11:49:49 -0700

    Prompt for how many days are worked in a month.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,4 +8,13 @@ console.log(name);
 //greet fellow driver and ask if they would like to see how many miles they will lyft in a year?
 alert("Greetings! "+name+" I hope you're well now let's see how many miles you will drive this year? Thanks!");
 
+//create a variable for how many times worked in a month.
+var daysWorked = prompt("How many days do you work in a month?");
+console.log(daysWorked);
+
+//create variable how many miles does the driver drive in a year?
+var milesDay = 50;
+
+
+
 
