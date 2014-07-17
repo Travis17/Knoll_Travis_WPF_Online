e639c965fc48f3271b7375412ef9b9ec4ea2cab3
Travis Knoll <trav_k80@yahoo.com>
2014-07-16 22:38:52 -0700

    Create a variable to see how many steps a person walks in a day.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,7 @@
 //Travis Knoll
 //Date 16 July 2014
 
-//Code to see how many miles will a person run in one year?
+//Code to see how many miles/steps a person walk in one year?
 
 //What is the persons name?
 var name = prompt("What is you name?");
@@ -10,5 +10,13 @@ console.log(name);
 //alert username and say Hi!
 alert("Hi "+name+", lets see how many steps you walk in a year?");
 
-
 //How many miles does a person walk a day?
+var milesDay = prompt("How many miles do you walk a day?");
+console.log(milesDay);
+
+//Create a variable for steps equal a mile?
+var stepsMile = 2500;
+alert("Wowzas you walk "+stepsMile * milesDay+" Steps daily. Now lets see for the year!");
+console.log(stepsMile * milesDay);
+
+
