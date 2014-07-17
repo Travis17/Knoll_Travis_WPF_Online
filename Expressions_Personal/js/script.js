f8fa68a93220b8aa87a46739251838ba13864f26
Travis Knoll <trav_k80@yahoo.com>
2014-07-16 22:43:42 -0700

    Create a variable to see how many steps a person takes in a year.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -16,7 +16,20 @@ console.log(milesDay);
 
 //Create a variable for steps equal a mile?
 var stepsMile = 2500;
+
+//alert for how many steps a person has taken in a day.
 alert("Wowzas you walk "+stepsMile * milesDay+" Steps daily. Now lets see for the year!");
 console.log(stepsMile * milesDay);
+
+//create a varuable to see how many steps a person walks in a year.
+var daysYear = 365
+
+
+
+
+
+
+
+
 
 
