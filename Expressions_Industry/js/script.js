03e0fa5b1abac427595bda86e565a1e3a7c75e89
Travis Knoll <trav_k80@yahoo.com>
2014-07-17 11:41:32 -0700

    Greet the fellow driver

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,3 +5,7 @@
 var name = prompt("What is your name and I can tell you how many miles you will drive for lyft in a year.");
 console.log(name);
 
+//greet fellow driver and ask if they would like to see how many miles they will lyft in a year?
+alert("Greetings! "+name+" I hope you're well now let's see how many miles you will drive this year? Thanks!");
+
+
