f18e83925cc1c16c1801a29044b29dd44944dbf2
Travis Knoll <trav_k80@yahoo.com>
2014-07-17 11:36:29 -0700

    Initial setup of assignment and problem of how many miles will a left driver drive in a year?

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,7 @@
 //Travis Knoll
-//Date 16 July 2014
+//Date: 17 July 2014
+
+//On average how many miles will a lyft driver drive for in a year.
+var name = prompt("What is your name and I can tell you how many miles you will drive for lyft in a year.");
+console.log(name);
 
