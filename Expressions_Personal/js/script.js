9050097e626ca8a417b026607e7c68135e789b66
Travis Knoll <trav_k80@yahoo.com>
2014-07-16 22:10:06 -0700

    How many miles will a person run in a year?

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,6 +1,8 @@
 //Travis Knoll
 //Date 16 July 2014
 
-//How many miles will a person run in one year?
+//Code to see how many miles will a person run in one year?
+
+
 var name = prompt("What is you name?");
 console.log(name);
