25a28f097300a7dd57150a85cd46ea613f6f150e
Travis Knoll <trav_k80@yahoo.com>
2014-07-16 22:21:14 -0700

    How many step does a person walk in a day?

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,6 +3,12 @@
 
 //Code to see how many miles will a person run in one year?
 
-
+//What is the persons name?
 var name = prompt("What is you name?");
 console.log(name);
+
+//alert username and say Hi!
+alert("Hi "+name+", lets see how many steps you walk in a year?");
+
+
+//How many miles does a person walk a day?
