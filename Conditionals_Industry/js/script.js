e87e8d25042e19a11d1c81e864e41941041021f3
Travis Knoll <trav_k80@yahoo.com>
2014-07-24 10:57:20 -0700

    Created a Conditional and else if for which phone to purchase on a persons budget.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -14,7 +14,15 @@ console.log(userIncome);
 var phone = 300;
 
 //Create a conditional for the person to know which operating system to buy.
-if(phone >= userInfo){
+if(phone > userInfo){
+	//alert you can buy the iPhone
 	alert("You can buy the iPhone or OS system");
 	console.log("You can buy the iPhone or OS system");
+}else if(userIncome + userInfo > phone){
+	//alert you can buy the Andriod phone
+	alert("You can buy the Andriod system Galaxy Note III");
+	console.log("You can buy the Andriod system Galaxy Note III")
+}else{
+	//Whever is left.
+	console.log("You need to get another job so you can get a better phone!");
 }
