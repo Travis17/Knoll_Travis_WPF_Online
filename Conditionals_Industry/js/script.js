0aa1b8fde9d54dbed84c9e9537aedd5aeb928617
Travis Knoll <trav_k80@yahoo.com>
2014-07-24 10:38:16 -0700

    Created a variable for a persons monthly income.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,3 +5,7 @@
 //Create a prompt to ask a person what there budget is.
 var userInfo = prompt("How much is your budget for you new cell phone?");
 console.log(userInfo);
+
+//What is your monthly income.
+var userIncome = prompt("What is you monthly income?");
+console.log(userIncome);
