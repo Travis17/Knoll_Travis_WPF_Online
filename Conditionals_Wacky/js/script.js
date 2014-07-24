451f4ee51cdbbf2c99c53c69db38f68b5ac6a068
Travis Knoll <trav_k80@yahoo.com>
2014-07-24 08:32:05 -0700

    Create 2nd prompt and changed variable options.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,8 +6,14 @@
 var personAge = prompt("What is your age so we can see what kind of movie you can watch?");
 console.log(personAge);
 
+//Prompt do you have kids?
+var numKids = prompt("How many kids do you have?");
+console.log(numKids);
+
 //Create variables for else if statement.
 var youngAge = 18;
 var midAge = 21;
 var oldAge = 50;
 
+//Create a conditional to show which type of movie rating to watch.
+if()
