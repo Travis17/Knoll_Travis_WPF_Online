bfdcfe88834fdbf269f612023ec2186382b5926e
Travis Knoll <trav_k80@yahoo.com>
2014-07-24 11:09:05 -0700

    Create a Variables for the operator.

--- Conditionals_Industry/js/script2.js
+++ Conditionals_Industry/js/script2.js
@@ -3,3 +3,15 @@
 //Conditional_Industry_Ternary
 
 //Create from the same assignment a Ternary operator
+
+
+//Create a prompt to ask a person what there budget is.
+var userInfo = prompt("How much is your budget for you new cell phone?");
+console.log(userInfo);
+
+//What is your monthly income.
+var userIncome = prompt("What is you monthly income?");
+console.log(userIncome);
+
+//Create a variable for value of the phone.
+var phone = 300;
