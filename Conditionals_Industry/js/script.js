59685f78f3598f07e164169244237b268e62598e
Travis Knoll <trav_k80@yahoo.com>
2014-07-24 10:35:09 -0700

    Created a problem for which cell phone a person should buy on their budget.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,7 @@
-alert("Last assignment")
+//Travis Knoll
+//Date 24 July 2014
+//Conditional_Industry
+
+//Create a prompt to ask a person what there budget is.
+var userInfo = prompt("How much is your budget for you new cell phone?");
+console.log(userInfo);
