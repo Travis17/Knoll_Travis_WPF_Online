f62c90e2654fae13be237b6bbe109734f05827c5
Travis Knoll <trav_k80@yahoo.com>
2014-07-23 21:53:01 -0700

    Created first prompt variable

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,3 +5,8 @@
 //If I ride my bike at least 20 miles a week I can eat a steak. 
 //If not I have to eat a salad.
 
+//Prompt given for how many miles ina week.
+var milesBike = prompt("How many miles did you ride you bike today?");
+console.log(milesBike);
+
+//Prompt given for how many miles in a week
