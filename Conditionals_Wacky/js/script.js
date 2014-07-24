43250296a6ef51037c557fd407f09bfaf7a43826
Travis Knoll <trav_k80@yahoo.com>
2014-07-23 23:35:00 -0700

    Prompt a person to see how old they are.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,3 +2,7 @@
 //Date: 22 July 2014
 //Conditional_Wacky 
 
+//Prompt person to ask age.
+var personAge = prompt("What is your age so we can see what kind of movie you can watch?");
+console.log(personAge);
+
