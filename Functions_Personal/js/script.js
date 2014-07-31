a06ab6f2a69b4da0ab10cf7775005c8313082d65
Travis Knoll <trav_k80@yahoo.com>
2014-07-31 15:21:32 -0700

    Create a variable to prompt a user their name.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,8 @@
-alert("hi");
+//Travis Knoll
+//Date: 31 July 2014
+
+//Create a Function to ask a person if they drink coffee and find out how many cups they will drink in a year.
+//Prompt to ask a person what there name is.
+
+var userInfo = prompt("What is you name?");
+console.log(userInfo);
