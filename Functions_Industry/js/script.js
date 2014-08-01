f7206bf478befe0bd6b1d798bd3bdd96167bcc53
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 15:13:10 -0700

    create a variable for the width of a persons smart phone.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,7 @@
 //create a problem. What is a person megapixels for their smart phone given their height and width.
 var userHeight = prompt("What is the height of your smart phone?");
 console.log(userHeight);
+
+//create a variable for the user width of the phone.
+var userWidth = prompt("What is the width of your smart phone?");
+console.log(userWidth);
