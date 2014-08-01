4f7e5e16805a42fb8533d0ccb4a8306613ac5869
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 15:05:56 -0700

    create a problem to solve for a persons smart phone megapixels.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,1 +1,5 @@
-alert("hi");
+//Travis Knoll
+//Date 1 Aug 2014
+//Assignment Function_Industry
+
+//create a problem. What is a person megapixels for their smart phone given their height and width.
