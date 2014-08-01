e723e19bb2013b3790e62beb8a466312cd13d37d
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 13:32:21 -0700

    Created the problem. How much wood can a woodchuck chuck?

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,7 @@
+//Travis Knoll
+//Date: 31 July 2014
+//Assignment: Function_Wacky
+
+//Create the problem to solve. How much wood could a woodchuck chuck if a woodchuck could chuck wood?
+var sayHi = prompt("Lets find out how much wood a woodchuck can chuck if a woodchuck could chuck wood.")
+
