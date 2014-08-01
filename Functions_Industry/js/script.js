bd1f6b9eff884bacdab1973be07ac0de26ee4ed4
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 15:15:21 -0700

    create an activate to work the function.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -9,3 +9,7 @@ console.log(userHeight);
 //create a variable for the user width of the phone.
 var userWidth = prompt("What is the width of your smart phone?");
 console.log(userWidth);
+
+//activate the function.
+var megaPixels = calculatemegaPixels(userHeight,userWidth);
+console.log(megaPixels);
