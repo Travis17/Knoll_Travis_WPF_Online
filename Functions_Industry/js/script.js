0c4be91821de9fa26e3479e64684678caf8f2b84
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 15:24:36 -0700

    create an if else statement to tell the user what they should do.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,11 +3,11 @@
 //Assignment Function_Industry
 
 //create a problem. What is a person megapixels for their smart phone given their height and width.
-var userHeight = prompt("What is the height of your smart phone?");
+var userHeight = prompt("What is the height of your smart phone, in inches?");
 console.log(userHeight);
 
 //create a variable for the user width of the phone.
-var userWidth = prompt("What is the width of your smart phone?");
+var userWidth = prompt("What is the width of your smart phone, in inches?");
 console.log(userWidth);
 
 //activate the function.
@@ -16,5 +16,12 @@ console.log(megaPixels);
 
 //create a function to calculate the persons megaPixels.
 function calculatemegaPixels(u,u){
-	return u * u;
+	return u * u * 35;
+}
+
+//create an if statement to let them know what they have.
+if(megaPixels > 350){
+	console.log("You must have a new phone");
+}else{
+	console.log("Perhaps it is time for an upgrade!")
 }
