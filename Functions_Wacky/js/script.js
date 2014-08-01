4b4fb4d72a5bd1eae03eb690111d6720d470d0fd
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 13:39:06 -0700

    Prompted user to see how many woodchucks reside in their area?

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,5 +3,8 @@
 //Assignment: Function_Wacky
 
 //Create the problem to solve. How much wood could a woodchuck chuck if a woodchuck could chuck wood?
-var sayHi = prompt("Lets find out how much wood a woodchuck can chuck if a woodchuck could chuck wood.")
-
+var sayHi = alert("Hi, lets find out how much wood a woodchuck can chuck if a woodchuck could chuck wood.");
+console.log("Hi, lets find out how much wood a woodchuck can chuck if a woodchuck could chuck wood?");
+//create a variable for user interaction and state how many wood chucks there will be.
+var userInfo = prompt("How many woodchucks are in your area?");
+console.log(userInfo);
