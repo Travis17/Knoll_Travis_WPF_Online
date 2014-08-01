c9ffb3f70c17347a101b996197c65181410634f1
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 15:07:38 -0700

    create a variable for the user height of phone.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,3 +3,5 @@
 //Assignment Function_Industry
 
 //create a problem. What is a person megapixels for their smart phone given their height and width.
+var userHeight = prompt("What is the height of your smart phone?");
+console.log(userHeight);
