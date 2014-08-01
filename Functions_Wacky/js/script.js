858a18b9705a377d40dd2174ca741a466aa11191
Travis Knoll <trav_k80@yahoo.com>
2014-08-01 13:52:14 -0700

    Create a variable for the number of logs mr woodchuck can chuck in a day.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,6 +5,13 @@
 //Create the problem to solve. How much wood could a woodchuck chuck if a woodchuck could chuck wood?
 var sayHi = alert("Hi, lets find out how much wood a woodchuck can chuck if a woodchuck could chuck wood.");
 console.log("Hi, lets find out how much wood a woodchuck can chuck if a woodchuck could chuck wood?");
+
 //create a variable for user interaction and state how many wood chucks there will be.
 var userInfo = prompt("How many woodchucks are in your area?");
 console.log(userInfo);
+
+//create a variable for the number logs a woodchuck can chuck in a day/
+var logsDay = 5;
+
+
+
